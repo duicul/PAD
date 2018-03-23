2bcdf708f59f4009a451924a94876b66ef0677f7
laur <duicul@yahoo.com>
2018-03-23 02:06:09 +0200

    gfd

---
+++ Web/src/Start.java
@@ -0,0 +1,45 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Start
+ */
+@WebServlet(description = "ceva", urlPatterns = { "/Start" })
+public class Start extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Start() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String docType ="<!doctype html public>\n";
+		String nume=request.getParameter("nume");
+        System.out.println(nume);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println("<div>Ceau acum "+nume+"</div>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
